0ca311a096e192904c7dabd3e679e81b672b9c2c
John EVANS (eva0034) <evansjohn@evansautoelectrics.com.au>
2024-01-06 17:11:49 +1100

    Update install.sh to utilize all cores while building:
    utilize all cores of the build system while compiling code as nproc detects the numbers of available cores

--- install.sh
+++ install.sh
@@ -208,7 +208,7 @@ if [ $pulseaudio = false ]
     sed -i 's/*imtu = 48;/*imtu = 60;/g' src/modules/bluetooth/backend-ofono.c
     sudo apt-get build-dep -y pulseaudio
     meson setup build
-    sudo ninja -C build
+    sudo ninja -C build -j$(nproc)
     sudo ninja -C build install
     sudo ldconfig
     # copy configs and force an exit 0 just in case files are identical (we don't care but it will make pimod exit)
@@ -254,7 +254,7 @@ if [ $bluez = false ]
     rm bluez-5.63.tar.xz
     cd bluez-5.63
     ./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --enable-library --disable-manpages --enable-deprecated
-    make
+    make -j$(nproc)
     sudo make install
     cd ..
 fi
@@ -308,7 +308,7 @@ else
   fi
 
   #beginning make
-  make -j2
+  make -j$(nproc)
 
   if [[ $? -eq 0 ]]; then
     echo -e Aasdk Make completed successfully '\n'
@@ -379,7 +379,7 @@ else
   fi
 
   #beginning make
-  make
+  make -j$(nproc)
 
   if [[ $? -eq 0 ]]; then
     echo -e h264bitstream Make completed successfully '\n'
@@ -464,7 +464,7 @@ if [ $gstreamer = true ]; then
   fi
 
   echo Making Gstreamer
-  make -j4
+  make -j$(nproc)
 
   if [[ $? -eq 0 ]]; then
     echo -e Gstreamer make ok'\n'
@@ -540,7 +540,7 @@ else
   fi
 
   echo Beginning openauto make
-  make
+  make -j$(nproc)
   if [[ $? -eq 0 ]]; then
     echo -e Openauto make OK'\n'
   else
@@ -589,7 +589,7 @@ else
   fi
 
   echo Running Dash make
-  make
+  make -j$(nproc)
   if [[ $? -eq 0 ]]; then
       echo -e Dash make ok, executable can be found ../bin/dash
       echo
